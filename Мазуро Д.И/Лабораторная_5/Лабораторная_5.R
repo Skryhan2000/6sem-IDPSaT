e722178e91cccef76d6d46d64f883c6d88edb209
didell2015 <didell2015@gmail.com>
2020-04-13 23:20:55 +0300

    5,6

---
+++ Мазуро Д.И/Лабораторная_5/Лабораторная_5.R
@@ -0,0 +1,16 @@
+# 1.создание вектора через seq
+myset<-seq(20)
+# 2.операции сложения, умножения вектора с числом.
+myset+1
+myset*2
+# 3.работу функций с ветором: 
+# sum, max, min, prod, length, mean, var, sort, summary
+sum(myset)
+max(myset)
+min(myset)
+prod(myset)
+length(myset)
+mean(myset)
+var(myset)
+sort(myset)
+summary(myset)
